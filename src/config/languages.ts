d5d0dbee23b06937c3f7828be74c6476050933e2
NoamTal <gg.el0ai.com@gmail.com>
2025-11-30 11:09:52 +0000

    Can you support the Yiddish language?

--- src/config/languages.ts
+++ src/config/languages.ts
@@ -5,15 +5,16 @@ export type Language = {
 
 export const LANGUAGES: Language[] = [
   { value: 'Arabic', label: 'Arabic' },
+  { value: 'Chinese (Simplified)', label: 'Chinese (Simplified)' },
+  { value: 'English', label: 'English' },
   { value: 'French', label: 'French' },
-  { value: 'Spanish', label: 'Spanish' },
   { value: 'German', label: 'German' },
+  { value: 'Hebrew', label: 'Hebrew' },
   { value: 'Italian', label: 'Italian' },
   { value: 'Japanese', label: 'Japanese' },
   { value: 'Korean', label: 'Korean' },
   { value: 'Portuguese', label: 'Portuguese' },
   { value: 'Russian', label: 'Russian' },
-  { value: 'Chinese (Simplified)', label: 'Chinese (Simplified)' },
-  { value: 'Hebrew', label: 'Hebrew' },
-  { value: 'English', label: 'English' },
+  { value: 'Spanish', label: 'Spanish' },
+  { value: 'Yiddish', label: 'Yiddish' },
 ];
