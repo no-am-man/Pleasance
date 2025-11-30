74ef01410fc667f588bda493aaa5fef2b9859355
NoamTal <gg.el0ai.com@gmail.com>
2025-11-30 23:55:07 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-2441219031-242ae",
+  "appId": "1:865373195178:web:194231a9d92b92c16979d3",
+  "apiKey": "AIzaSyCMDygvQZnjbVLtr9RQn0IT2p4-STcHRk8",
+  "authDomain": "studio-2441219031-242ae.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "865373195178"
+};
