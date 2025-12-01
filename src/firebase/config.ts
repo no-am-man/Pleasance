e8a382817bd8342f387924de2211d042c3f62631
NoamTal <gg.el0ai.com@gmail.com>
2025-12-01 04:55:52 +0000

    Generation Failed Story creation failed. Firebase Storage: No default bu

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -3,6 +3,7 @@ export const firebaseConfig = {
   "appId": "1:865373195178:web:194231a9d92b92c16979d3",
   "apiKey": "AIzaSyCMDygvQZnjbVLtr9RQn0IT2p4-STcHRk8",
   "authDomain": "studio-2441219031-242ae.firebaseapp.com",
+  "storageBucket": "studio-2441219031-242ae.appspot.com",
   "measurementId": "",
   "messagingSenderId": "865373195178"
 };
