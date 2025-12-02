46a6fb6d3d85a55c5065b0c7af5b6a92ccc65de0
NoamTal <gg.el0ai.com@gmail.com>
2025-12-02 23:18:04 +0000

    For now, lets remove SVG3D menu item and will work on it later

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -17,7 +17,6 @@ import {
   Warehouse,
 } from 'lucide-react';
 import { Logo } from '@/components/icons';
-import { Svg3dCube } from '@/components/icons/svg3d-cube';
 import { Button } from '@/components/ui/button';
 import {
   Sheet,
@@ -48,7 +47,6 @@ const navLinks = [
   { href: '/story', label: 'Nuncy Lingua', icon: BookOpen },
   { href: '/fabrication', label: 'Fabrication', icon: Warehouse },
   { href: '/treasury', label: 'Treasury', icon: Banknote },
-  { href: '/svg3d', label: 'SVG3D', icon: Svg3dCube },
   { href: '/wiki', label: 'Wiki', icon: Info },
 ];
 
