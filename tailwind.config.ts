cccd05b03eb54c4e0213b956249c8c0c60a111de
NoamTal <gg.el0ai.com@gmail.com>
2025-12-05 17:10:31 +0000

    ./src/app/globals.css.webpack[javascript/auto]!=!./node_modules/next/dis

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,6 +1,7 @@
+
 import type {Config} from 'tailwindcss';
 
-export default {
+const config: Config = {
   darkMode: ['class'],
   content: [
     './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
@@ -99,4 +100,6 @@ export default {
     },
   },
   plugins: [require('tailwindcss-animate')],
-} satisfies Config;
+};
+
+export default config;
