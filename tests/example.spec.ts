f542ee2f781708d7f5b6243261d2eae1c0fa7592
NoamTal <gg.el0ai.com@gmail.com>
2025-12-05 20:35:41 +0000

    npm run test:e2e

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,18 +1,18 @@
 import { test, expect } from '@playwright/test';
 
-// test('has title', async ({ page }) => {
-//   await page.goto('/');
+test('has title', async ({ page }) => {
+  await page.goto('/');
 
-//   // Expect a title "to contain" a substring.
-//   await expect(page).toHaveTitle(/Pleasance/);
-// });
+  // Expect a title "to contain" a substring.
+  await expect(page).toHaveTitle(/Pleasance/);
+});
 
-// test('get started link', async ({ page }) => {
-//   await page.goto('/');
+test('get started link', async ({ page }) => {
+  await page.goto('/');
 
-//   // Click the get started link.
-//   await page.getByRole('link', { name: 'Explore Communities' }).click();
+  // Click the get started link.
+  await page.getByRole('link', { name: 'Explore Communities' }).click();
 
-//   // Expects the URL to contain intro.
-//   await expect(page).toHaveURL(/.*community/);
-// });
+  // Expects the URL to contain intro.
+  await expect(page).toHaveURL(/.*community/);
+});
