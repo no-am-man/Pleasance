ac753d3c08cf85335e9142990663417b57a83c92
NoamTal <gg.el0ai.com@gmail.com>
2025-12-05 20:00:51 +0000

    pleasance@8.0.0 test:e2e > playwright test [WebServer]  ⨯ Unable to acqu

--- playwright.config.ts
+++ playwright.config.ts
@@ -41,11 +41,4 @@ export default defineConfig({
       use: { ...devices['Desktop Safari'] },
     },
   ],
-
-  /* Run your local dev server before starting the tests */
-  webServer: {
-    command: 'npm run dev',
-    url: 'http://localhost:3000',
-    reuseExistingServer: !process.env.CI,
-  },
 });
