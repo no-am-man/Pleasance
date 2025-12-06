8c1a76a85a77243e48babda1a88696fc93e0efc5
NoamTal <gg.el0ai.com@gmail.com>
2025-12-06 06:02:05 +0000

    Try fixing this error: `Build Error: Error evaluating Node.js code. Erro

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,7 +1,11 @@
+
 /** @type {import('postcss-load-config').Config} */
 const config = {
   plugins: {
-    '@tailwindcss/postcss': {},
+    '@tailwindcss/postcss': {
+      // Add the path to your tailwind.config.ts file.
+      config: './tailwind.config.ts',
+    },
     autoprefixer: {},
   },
 };
