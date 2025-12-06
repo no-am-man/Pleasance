64b9f5465f549e5a64c2cae605c8a1e31e15eba4
NoamTal <gg.el0ai.com@gmail.com>
2025-12-06 06:48:08 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/components/icons/index.ts
+++ src/components/icons/index.ts
@@ -4,19 +4,18 @@ export function Logo(props: SVGProps<SVGSVGElement>) {
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
-      width="24"
-      height="24"
       viewBox="0 0 24 24"
       fill="none"
       stroke="currentColor"
-      strokeWidth="2"
+      strokeWidth="1.5"
       strokeLinecap="round"
       strokeLinejoin="round"
       {...props}
     >
-      <circle cx="12" cy="12" r="10" />
-      <path d="M16 8h-6a2 2 0 1 0 0 4h4a2 2 0 1 1 0 4H8" />
-      <path d="M12 4v16" />
+      <path d="M4 19.5A2.5 2.5 0 0 1 6.5 17H20" />
+      <path d="M6.5 2H20v15H6.5A2.5 2.5 0 0 1 4 14.5V4" />
+      <path d="m14 6-4.5 6 4.5 6" />
+      <path d="m18 6-4.5 6 4.5 6" />
     </svg>
   );
 }
