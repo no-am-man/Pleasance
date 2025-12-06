e2bc0f6fbae7e97722c01ac02ea1025697d3f9f6
NoamTal <gg.el0ai.com@gmail.com>
2025-12-06 06:54:03 +0000

    The error `Expected '>', got 'xmlns'` happens because **you are using JS

--- src/components/icons/index.ts
+++ src/components/icons/index.ts
@@ -1,25 +1,2 @@
-import type { SVGProps } from 'react';
 
-export function Logo(props: SVGProps<SVGSVGElement>) {
-  return (
-    <svg
-      viewBox="0 0 24 24"
-      fill="none"
-      stroke="currentColor"
-      strokeWidth="1.5"
-      strokeLinecap="round"
-      strokeLinejoin="round"
-      {...props}
-    >
-      {/* Budding Leaf */}
-      <path d="M12 2a4 4 0 0 0-4 4c0 1.1.4 2.1 1 3" />
-      <path d="M12 2a4 4 0 0 1 4 4c0 1.1-.4 2.1-1 3" />
-      
-      {/* Open Book */}
-      <path d="M2 3h6a4 4 0 0 1 4 4v14a3 3 0 0 0-3-3H2z" />
-      <path d="M22 3h-6a4 4 0 0 0-4 4v14a3 3 0 0 1 3-3h7z" />
-    </svg>
-  );
-}
-
-export * from './svg3d-cube';
+// This file has been moved to index.tsx to support JSX.
