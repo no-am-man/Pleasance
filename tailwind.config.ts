9dac94c7361fac706c364c754262bef8fcdf5009
NoamTal <gg.el0ai.com@gmail.com>
2025-12-06 06:00:23 +0000

    Try fixing this error: `Build Error: Error evaluating Node.js code. Erro

---
+++ tailwind.config.ts
@@ -0,0 +1,18 @@
+import type { Config } from 'tailwindcss';
+
+const config: Config = {
+  darkMode: ['class'],
+  content: ['./src/**/*.{ts,tsx}'],
+  theme: {
+    container: {
+      center: true,
+      padding: '2rem',
+      screens: {
+        '2xl': '1400px',
+      },
+    },
+  },
+  plugins: [],
+};
+
+export default config;
